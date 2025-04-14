11fea55d699078553a3f663c1b1ebd8dbc817329
masoud shahrian <161046359+masoudshahrian@users.noreply.github.com>
2025-04-14 12:22:41 +0000

    "v3"

--- packaging-ml-model/prediction_model/config/config.py
+++ packaging-ml-model/prediction_model/config/config.py
@@ -10,3 +10,33 @@ TRAIN_FILE='train.csv'
 TEST_FILE='test.csv'
 
 SAVE_MODE_PATH= os.Path.join(PACKAGE_ROOT,'trained_models')
+
+TARGET = 'Loan_Status'
+
+# final features used in the model
+FEATURES=['Gender', 'Married', 'Dependents', 'Education','Self_Employed',
+'ApplicantIncome','LoanAmount','Loan_Amount_Term','Credit_History', 'Property_Area']
+
+
+NUM_FEATURES=['ApplicantIncome','LoanAmount','Loan_Amount_Term']
+
+CAT_FEATURES =['Gender', 'Married', 'Dependents', 'Education','Self_Employed',
+'Credit_History', 'Property_Area']
+
+
+# this is same as CAT_FEATURES
+FEATURES_TO_ENDCODE=['Gender', 'Married', 'Dependents', 'Education','Self_Employed',
+'Credit_History', 'Property_Area']
+
+FEATURE_TO_MODIFY=['ApplicantIncome']
+
+FEATURE_TO_ADD=['CoapplicantIncome']
+
+DROPED_FEATURES=['CoapplicantIncome']
+
+LOG_FEATURES=['ApplicantIncome', 'LoanAmount'] #taking log on numerical columns
+
+
+
+
+
